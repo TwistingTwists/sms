f5df9a4b020193e3e679ec1303f8952f493ea425
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2015-11-30 23:05:02 +0530

    modified send.py according to data/config.json

--- sms/send.py
+++ sms/send.py
@@ -1,27 +1,37 @@
 from twilio.rest import TwilioRestClient
 from twilio.rest.resources import Connection
 import json
+from data import module_locator
 
-with open('../config.json','r') as f:
-    confif = json.load(f)
+json_path = module_locator.modeule_path()
 
-from twilio.rest.resources.connection  import conf['proxy']['ProxyType']
+# with open("./"+json_path+"/"+'config.json','r') as f:
+with open(json_path+"/"+'config.json','r') as f:
+    conf = json.load(f)
+
+from twilio.rest.resources.connection  import PROXY_TYPE_SOCKS5, PROXY_TYPE_HTTP
 import sys
 
 
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = config['auth']['AuthSid']
-auth_token  = config['auth']['AuthToken']
+account_sid = conf['auth']['AuthSid']
+auth_token  = conf['auth']['AuthToken']
 
 #-----------------------------------------------------------------------------------------
 # Proxy settings, like you want, use PROXY_TYPE_SOCKS5 for socks proxy
-if conf['proxy']['use_proxy']:
+if (conf['proxy']['ProxyType'] == "HTTP"):
     Connection.set_proxy_info(
-        conf['proxy']['Proxy'],  #proxy server
-        conf['proxy']['Port'],               #port
-        proxy_type=conf['proxy']['ProxyType']
+        str(conf['proxy']['Proxy']),
+        conf['proxy']['Port'],
+        proxy_type=PROXY_TYPE_HTTP,
         )
+
+else:
+    Connection.set_proxy_info(
+        str(conf['proxy']['Proxy']),  #proxy server
+        conf['proxy']['Port'],               #port
+        proxy_type=PROXY_TYPE_SOCKS5)
 #-----------------------------------------------------------------------------------------
 
 client = TwilioRestClient(account_sid, auth_token)
@@ -30,27 +40,11 @@ def print_error():
     print("Usage : sms [name] ['message']")
     print("Define `name` in `config.json`. ")
 
-def send_message(num,text=conf['user']['message']):
+def send_message(num,text=conf['user']['default_message']):
     message = client.messages.create(body=text,
-        to=num,                                     # Replace with your phone number
-        from_= conf['personal']['your_twilio_number']             # Replace with your Twilio number
-    print ("[{}] -> {} ".format(message.sid,message.body))
-
-def  main_oldy():
-    """
-    This is the old implementation.
-    usage : sms -n me -t "forget to eat!"
-    """
-    command = sys.argv[1:]
-    if not command:
-        print_error()
-        sys.exit(2)
-
-    if command[0] == '-n':
-        num = who(command[1])
-    if command[2] == '-t':
-        text = command[3]
-    send_message(num,text)
+        to=num,
+        from_= conf['personal']['your_twilio_number']    )
+    print ("[{}] -> {} to {}".format(message.sid,message.body,num))
 
 
 
@@ -65,9 +59,10 @@ def  main():
         print_error()
         sys.exit(2)
     print (command[0])
+    s = command[0]
     num = []
     text = []
-    # num = who(command[0])
+
     num = conf['personal']['verified_numbers'][s]
 
     ## in case I cannot find whose number, revert the message to you ;)
@@ -76,6 +71,6 @@ def  main():
         num = conf['personal']['your_twilio_number']          #your number
     text = command[1]
 
-    print ("{} to {}.".format(text,num))
+#    print ("{} to {}.".format(text,num))
     send_message(num,text)
 
